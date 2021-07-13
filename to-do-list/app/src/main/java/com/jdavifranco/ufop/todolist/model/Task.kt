182345b1b1b238378b86dfeb9edffc0ff7795918
Joao-Davi-Franco <developerjdavi@gmail.com>
2021-07-13 17:29:43 -0300

    Model de task criado

--- to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/Task.kt
+++ to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/Task.kt
@@ -1,4 +1,11 @@
 package com.jdavifranco.ufop.todolist.model
-
-class Task {
-}
+/*
+Data class são classes criadas com o único objetivo de guardar dados, nesse caso
+os dados das tarefas.
+A data classe task define apenas as propriedades da tarefa e um construtor
+ */
+data class Task(
+    val name:String,
+    val date:String,
+    val hour:String
+)
