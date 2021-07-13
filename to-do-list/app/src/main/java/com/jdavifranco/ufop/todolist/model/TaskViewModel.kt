1ed15c760652274fdd10e2814e3f443a8ed1a001
Joao-Davi-Franco <developerjdavi@gmail.com>
2021-07-13 17:48:34 -0300

    Layout terminado

--- to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/TaskViewModel.kt
+++ to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/TaskViewModel.kt
@@ -1,4 +1,12 @@
 package com.jdavifranco.ufop.todolist.model
 
-class TaskViewModel {
+import androidx.lifecycle.ViewModel
+
+/*
+Seguindo a arquitetura MVVM(Model View viewmodel)
+será utilizo um viewmodel para acessar os dados das tasks
+e fazer operações sobre esses dados
+ */
+class TaskViewModel: ViewModel() {
+
 }
