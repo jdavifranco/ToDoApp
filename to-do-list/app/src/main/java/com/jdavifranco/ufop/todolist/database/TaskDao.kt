f5445840d1bbf7375150a12e80aea399b2db0b63
jdavifranco <developerjdavi@gmail.com>
2021-07-14 09:05:45 -0300

    add project and module dependencies

--- to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/database/TaskDao.kt
+++ to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/database/TaskDao.kt
@@ -1,4 +1,10 @@
 package com.jdavifranco.ufop.todolist.database
+/*
+Nesse projeto, como melhora no original será adicionado um banco de dados local
+utilizando a biblioteca jetpack Room.
+ */
 
-class TaskDao {
+interface TaskDao {
+
+
 }
