5dd860259872fa1810580c1a9dd83a106eb2d98d
jdavifranco <developerjdavi@gmail.com>
2021-07-14 09:48:05 -0300

    Defined Room Classes

--- to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/database/TaskDao.kt
+++ to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/database/TaskDao.kt
@@ -1,10 +1,35 @@
 package com.jdavifranco.ufop.todolist.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.jdavifranco.ufop.todolist.model.Task
+
 /*
 Nesse projeto, como melhora no original será adicionado um banco de dados local
 utilizando a biblioteca jetpack Room.
  */
-
+/*
+A Dao siginfica Data Access object, é uma interdace contendo os
+métodos de acesso ao banco de dados, queries e etc.
+Essa terá apenas um crud básico
+ */
+/*É Necessário usar as anotações da biblioteca Room para que ela
+indetifique a Dao
+ */
+@Dao
 interface TaskDao {
-
-
+    /*
+    Com a anotação @Insert posso definir que essa função que adiciona uma task ao banco de dados
+     */
+    @Insert
+    fun insert(task: Task)
+    /*
+    A anotação @Query serve para definir uma consulta mais específica ao banco de dados
+    Utilizando linguagem SQL
+     */
+    @Query("SELECT * FROM tableTasks")
+    fun getAllTasks():LiveData<List<Task>>
 }
--- to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/Task.kt
+++ to-do-list/app/src/main/java/com/jdavifranco/ufop/todolist/model/Task.kt
@@ -1,10 +1,16 @@
 package com.jdavifranco.ufop.todolist.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /*
 Data class são classes criadas com o único objetivo de guardar dados, nesse caso
 os dados das tarefas.
 A data classe task define apenas as propriedades da tarefa e um construtor
  */
+@Entity(tableName = "tableTasks")
 data class Task(
+    @PrimaryKey
     val name:String,
     val date:String,
     val hour:String
